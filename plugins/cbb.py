985544e46e0507beff5637338cce46c7360ec97c
freefiresingapore24535-alt <freefiresingapore24535@gmail.com>
2025-11-22 22:53:05 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -68,14 +68,14 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             photo=QR_PIC,
             caption=(
                 f"👋 {query.from_user.username}\n\n"
-                f"🎖️ Available Plans :\n\n"
-                f"● {PRICE1}  For 0 Days Prime Membership\n\n"
-                f"● {PRICE2}  For 1 Month Prime Membership\n\n"
-                f"● {PRICE3}  For 3 Months Prime Membership\n\n"
-                f"● {PRICE4}  For 6 Months Prime Membership\n\n"
-                f"● {PRICE5}  For 1 Year Prime Membership\n\n\n"
-                f"💵 ASK UPI ID TO ADMIN AND PAY THERE -  <code>{UPI_ID}</code>\n\n\n"
-                f"♻️ After Payment You Will Get Instant Membership \n\n\n"
+                f"🎖️ Available Plans :\n"
+                f"● {PRICE1}  For 7 Days Prime Membership\n"
+                f"● {PRICE2}  For 1 Month Prime Membership\n"
+                f"● {PRICE3}  For 3 Months Prime Membership\n"
+                f"● {PRICE4}  For 6 Months Prime Membership\n"
+                f"● {PRICE5}  For 1 Year Prime Membership\n\n"
+                f"💵 ASK UPI ID TO ADMIN AND PAY THERE -  <code>{UPI_ID}</code>\n\n"
+                f"♻️ After Payment You Will Get Instant Membership \n\n"
                 f"‼️ Must Send Screenshot after payment & If anyone want custom time membrship then ask admin"
             ),
             reply_markup=InlineKeyboardMarkup(
