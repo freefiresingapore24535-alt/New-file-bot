055995db51ed9f4e031abdcbe7ce6e2237c9eca0
freefiresingapore24535-alt <freefiresingapore24535@gmail.com>
2025-11-23 13:58:59 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -188,33 +188,18 @@ async def start_command(client: Client, message: Message):
             except Exception as e:
                 print(f"Error updating notification with 'Get File Again' button: {e}")
     else:
-        reply_markup = InlineKeyboardMarkup(
-            [        
-    [
-                    InlineKeyboardButton("• ᴀʙᴏᴜᴛ •", callback_data = "about"),
-                    InlineKeyboardButton('• ᴄʟᴏsᴇ •', callback_data = "close")
-    ],
-            
-                [InlineKeyboardButton("• ᴅᴇᴠᴇʟᴏᴘᴇʀ •", url="https://t.me/Minato_Sencie")]
-            ]
-        )
-        if query.from_user.id in client.admins:
-            buttons.insert(
-                0,
-                [
-                    InlineKeyboardButton(
-                        "⛩️ ᴄᴏᴍᴍᴀɴᴅꜱ ⛩️", callback_data="help"
-                    )
-                ],
-            )
-        await message.reply_photo(
-            photo=START_PIC,
-            caption=START_MSG.format(
-                first=message.from_user.first_name,
-                last=message.from_user.last_name,
-                username=None if not message.from_user.username else '@' + message.from_user.username,
-                mention=message.from_user.mention,
-                id=message.from_user.id
+        buttons = [[InlineKeyboardButton("• ᴀʙᴏᴜᴛ •", callback_data="about"), InlineKeyboardButton("• ᴄʟᴏsᴇ •", callback_data='close')],
+        [InlineKeyboardButton("• ᴅᴇᴠᴇʟᴏᴘᴇʀ •", url="https://t.me/Minato_Sencie"]]
+        if user_id in client.admins:
+            buttons.insert(0, [InlineKeyboardButton("⛩️ ᴄᴏᴍᴍᴀɴᴅꜱ ⛩️", callback_data="help")])
+
+        photo = client.messages.get("START_PHOTO", "")
+        start_caption = client.messages.get('START', 'Welcome, {mention}').format(
+            first=message.from_user.first_name,
+            last=message.from_user.last_name,
+            username=None if not message.from_user.username else '@' + message.from_user.username,
+            mention=message.from_user.mention,
+            id=message.from_user.id
             ),
             reply_markup=reply_markup,
             message_effect_id=5046509860389126442)  # 🎉
