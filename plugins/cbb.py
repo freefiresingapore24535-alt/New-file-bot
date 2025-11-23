7d0727bbbc5c81a7af9e11836ee2ac04622367ad
freefiresingapore24535-alt <freefiresingapore24535@gmail.com>
2025-11-23 09:53:20 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -17,23 +17,45 @@ from database.database import *
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
 
-    if data == "start":
-        await query.message.edit_text(
-            text=START_MSG.format(first=query.from_user.first_name),
-            disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton("• ᴀʙᴏᴜᴛ •", callback_data='about'),
-                 InlineKeyboardButton("• ᴄʟᴏsᴇ •", callback_data='close')],
-                [InlineKeyboardButton("• ᴅᴇᴠᴇʟᴏᴘᴇʀ •", url="https://t.me/Minato_Sencie")]
-            ])
-        )
+    @Client.on_callback_query(filters.regex('^home$'))
+async def home(client: Client, query: CallbackQuery):
+    buttons = [
+            [InlineKeyboardButton("• ᴀʙᴏᴜᴛ •", callback_data="about"), 
+             InlineKeyboardButton("• ᴄʟᴏsᴇ •", callback_data='close')
+            ], [InlineKeyboardButton("• ᴅᴇᴠᴇʟᴏᴘᴇʀ •", url="https://t.me/Minato_Sencie")
+               ]
+        ]
+    if query.from_user.id in client.admins:
+        buttons.insert(0, [InlineKeyboardButton("⛩️ ᴄᴏᴍᴍᴀɴᴅꜱ ⛩️", callback_data="help")])
+    await query.message.edit_text(
+        text=client.messages.get('START', 'No Start Message').format(
+            first=query.from_user.first_name,
+            last=query.from_user.last_name,
+            username=None if not query.from_user.username else '@' + query.from_user.username,
+            mention=query.from_user.mention,
+            id=query.from_user.id
+                
+        ),
+        reply_markup=InlineKeyboardMarkup(buttons)
+    )
     
-    elif data == "about":
+    if data == "about":
         await query.message.edit_text(
             text=ABOUT_TXT.format(first=query.from_user.first_name),
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton('• ʜᴏᴍᴇ •', callback_data='start'),
+                [InlineKeyboardButton('• ʜᴏᴍᴇ •', callback_data='home'),
+                 InlineKeyboardButton('• ᴄʟᴏsᴇ •', callback_data='close')],
+                [InlineKeyboardButton("• ᴅᴇᴠᴇʟᴏᴘᴇʀ •", url="https://t.me/Minato_Sencie")]
+            ])
+        )
+
+    if data == "help":
+        await query.message.edit_text(
+            text=HELP_TXT.format(first=query.from_user.first_name),
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup([
+                [InlineKeyboardButton('• ʜᴏᴍᴇ •', callback_data='home'),
                  InlineKeyboardButton('• ᴄʟᴏsᴇ •', callback_data='close')],
                 [InlineKeyboardButton("• ᴅᴇᴠᴇʟᴏᴘᴇʀ •", url="https://t.me/Minato_Sencie")]
             ])
